7e72ac506c7d18030543f0f26f8a401b4bd63507
zulaymejia <zulaymejia06@gmail.com>
2022-09-13 11:56:43 -0500

    Creación de componentes

---
+++ src/components/NavBarPufi/NavBarPufi.jsx
@@ -0,0 +1,8 @@
+//function NavBarPufi () {
+  //  return (
+          //<div className="home">
+            //  <h1>¡Queremos escucharte!</h1>           
+          //</div>
+      //);
+  //}
+  //export default NavBarPufi;
